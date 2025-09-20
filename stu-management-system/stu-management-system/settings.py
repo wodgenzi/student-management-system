b58f9e5d3d99992bd74d3fa3d98fa3c02ef20084
wodgenzi_ <107025355+wodgenzi@users.noreply.github.com>
2025-09-20 22:16:21 +0500

    Load environment variables and update secret key handling
    Added python-dotenv to requirements and updated settings.py to load environment variables using dotenv. The Django secret key is now read from the environment instead of being hardcoded, and ALLOWED_HOSTS was restricted to localhost for improved security.

--- stu-management-system/stu-management-system/settings.py
+++ stu-management-system/stu-management-system/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os.path
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m)92@e=d%^e5+9ia1p=&9!7ysd*7!^ngzx1e_0iu4lqzoh+845'
+
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["10.1.127.226", "127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
